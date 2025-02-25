4a42c19c5c20d08db4e0aa86f70f5b0476988f88
Keerthana-SV <keerthanasv23@gmail.com>
2025-02-25 08:17:33 +0530

    Create app.js

---
+++ Week5/React/task2/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Flower from './Flower'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Flower name="Rose" /> 
+    </div>
+  );
+}
+
+export default App;
