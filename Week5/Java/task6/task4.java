e9de15402a21692afd60eb981a7367c4ef5b5fef
Keerthana-SV <keerthanasv23@gmail.com>
2025-02-25 13:25:58 +0530

    Update task4.java

--- Week5/Java/task6/task4.java
+++ Week5/Java/task6/task4.java
@@ -1,67 +1,26 @@
-package org.example.assignment.corejava.Task5;
+package org.example.assignment.corejava.Task6;
 
-class MatrixMulThread extends Thread {
-    int[][] A, B, C;
-    int row, col, size;
+import java.util.ArrayList;
+import java.util.List;
 
-    MatrixMulThread(int[][] A, int[][] B, int[][] C, int row, int col, int size) {
-        this.A = A;
-        this.B = B;
-        this.C = C;
-        this.row = row;
-        this.col = col;
-        this.size = size;
-    }
-
-    public void run() {
-        for (int k = 0; k < size; k++) {
-            C[row][col] += A[row][k] * B[k][col];
+class task4 {
+    public static <T> List<T> reverseList(List<T> list) {
+        List<T> reversedList = new ArrayList<>();
+        
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reversedList.add(list.get(i));
         }
+        
+        return reversedList;
     }
-}
 
-public class task4 {
     public static void main(String[] args) {
-        int[][] A = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
+        List<Integer> intList = List.of(1, 2, 3, 4, 5);
+        System.out.println("Original Integer List: " + intList);
+        System.out.println("Reversed Integer List: " + reverseList(intList));
 
-        int[][] B = {
-            {1, 8, 5},
-            {7, 2, 4},
-            {6, 9, 3}
-        };
-
-        int size = A.length;
-        int[][] C = new int[size][size];
-
-        Thread[][] threads = new Thread[size][size];
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                threads[i][j] = new MatrixMulThread(A, B, C, i, j, size);
-                threads[i][j].start();
-            }
-        }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                try {
-                    threads[i][j].join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        System.out.println("Result Matrix:");
-        for (int[] row : C) {
-            for (int val : row) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-        }
+        List<String> strList = List.of("apple", "lime", "cherry", "plum");
+        System.out.println("\nOriginal String List: " + strList);
+        System.out.println("Reversed String List: " + reverseList(strList));
     }
 }
