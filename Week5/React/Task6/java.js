377522a506e2f898eb8a81d9954538eed313d3b9
Keerthana-SV <keerthanasv23@gmail.com>
2025-02-25 11:26:47 +0530

    Create java.js

---
+++ Week5/React/Task6/java.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Java() {
+  return (
+    <div className="topic">
+      <h2>Java</h2>
+      <p>Java is a popular, class-based, object-oriented programming language.</p>
+    </div>
+  );
+}
+
+export default Java;
