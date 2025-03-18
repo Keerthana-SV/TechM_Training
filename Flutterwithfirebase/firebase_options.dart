5e87f2ff95d412ed0d717873f27ca82ae9c15d6b
Keerthana-SV <keerthanasv23@gmail.com>
2025-03-18 17:26:58 +0530

    Create firebase_options.dart

---
+++ Flutterwithfirebase/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return FirebaseOptions(
+      apiKey: "AIzaSyDPliWv_b6jkKVS3v6lbyS9aHVykbkK6ZA",
+      authDomain: "safetrack-174da.firebaseapp.com",
+      projectId: "safetrack-174da",
+      storageBucket: "safetrack-174da.appspot.com",
+      messagingSenderId: "745254754331",
+      appId: "1:745254754331:android:91d47d5b79ba0fbafa80be",
+    );
+  }
+}
