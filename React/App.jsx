9e000a9699ca7be29f9f612919ae72eef2181a05
Keerthana-SV <keerthanasv23@gmail.com>
2025-03-27 08:33:42 +0530

    Add files via upload

--- React/App.jsx
+++ React/App.jsx
@@ -1,11 +1,13 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from "./components/contact"; 
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";  
 import RoomList from "./components/Hotels";
-import BookingForm from "./components/Form";
+import BookingForm from "./components/form"; // Correct casing
+
 
 const App = () => {
   const [rooms, setRooms] = useState([]);
@@ -17,13 +19,14 @@ const App = () => {
   }, []);
 
   return (
-    <Router> 
+    <Router>  {/* ✅ Wrap everything inside Router */}
       <Navbar />
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} /> {/* ✅ Home loads first */}
           <Route path="/hotels" element={<RoomList rooms={rooms} />} />
           <Route path="/form" element={<BookingForm />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
